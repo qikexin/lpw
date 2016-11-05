2fa775aacd0ea17c081ea4b6f9ca349c9b1f5f9e
lpw <442406570@qq.com>
2016-11-05 21:40:02 +0800

    用户登录练习

---
+++ uniqueword.py
@@ -0,0 +1,19 @@
+#author:lpw
+#date:
+
+import  string
+import  sys
+
+dict={1:"s",2:"sd",3:"w"}
+a=dict.keys()
+#dict.get(1)
+#print(dict.get(1))
+#a=[]
+#for key in dict.keys():
+#    a.append(key)
+
+print(a)
+if  in a:
+    print("yes")
+else:
+    print("no")
